a4994e94da5dad862d8240d53779fe76d9670afc
Dylan Beattie <dylan@dylanbeattie.net>
2022-11-16 12:01:02 +0000

    Placeholder for unit tests

---
+++ 10-unit-testing/counter/tests/counting-engine.test.js
@@ -0,0 +1,30 @@
+import { describe, test, expect } from 'vitest';
+import CountingEngine from '../counting-engine.js';
+
+test('it works', () => {
+    expect(1).toBe(1);
+});
+
+describe('engine initialises with correct count', () => {
+    let values = [0,5,10,15,20];
+    test.each(values)('engine initialises with %d correctly', (count) => {
+        let e = new CountingEngine(count,10);
+        expect(e.count).toBe(count);
+    });
+});
+
+describe('engine resets to correct value', () => {
+    let values = [0,5,10,15,20];
+    test.each(values)('engine resets when default is %d', (value) => {
+        let e = new CountingEngine(0,value);
+        e.reset();
+        expect(e.count).toBe(value);
+    });
+});
+
+//TODO:
+// Unit test for increment
+// Unit test for decrement
+
+
+
